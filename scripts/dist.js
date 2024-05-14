7fbfa4bdf677b5921d5001f71ccf6a908d175713
Julien ETIENNE <julien.etienne@intuiquest.com>
2024-05-14 15:07:59 +0200

    update path dist

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,6 +1,6 @@
 const pkg = require('../package.json');
 
-const target = `${pkg.name}@v${pkg.version}.osf`;
+const target = `${pkg.name}.osf`;
 
 console.log(`rm -f ./dist/${target}`);
 console.log(`cd ./src && zip -x **/.DS_Store -x .DS_Store -r ../dist/${target} .`);
